8ee15dc48724f3fcba4faec08a1e1631a8ca7d74
eric kyania <erickyania0@gmail.com>
2023-08-24 21:12:55 +0300

    created header,speakertoolbar components

--- src/components/speakers.js
+++ src/components/speakers.js
@@ -1,8 +1,12 @@
 import { data } from "../../SpeakerData";
-import Speaker from "./speaker";
+import SpeakerList from "./SpeakerList";
+import Header from "./Header";
+import SpeakerToolbar from "./speakertoolbar";
 const Speakers = () => {
   return (
     <div className="conatiner-fluid">
+        <Header></Header>
+        <SpeakerToolbar></SpeakerToolbar>
       <SpeakerList data={data}></SpeakerList>
     </div>
   );
